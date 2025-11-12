aadad62d10bb7f5cef4ea6cedccf9c1680ae275a
Zvonimir <zvonimir.stracenski@fer.hr>
2025-11-12 23:38:12 +0100

    Promijenjena statična ruta.

--- my-react-app/server.js
+++ my-react-app/server.js
@@ -411,7 +411,7 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
