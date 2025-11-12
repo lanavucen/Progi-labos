22f2073c5c3b74b39afb21618a7037a822cc95cb
Zvonimir <zvonimir.stracenski@fer.hr>
2025-11-12 23:43:10 +0100

    Statična ruta s regexom.

--- my-react-app/server.js
+++ my-react-app/server.js
@@ -411,10 +411,11 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('/*', (req, res) => {
+  app.get(/^\/(?!api).*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
+
 
 
 
