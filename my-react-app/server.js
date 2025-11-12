ec74468e23923d8ffe487b99b4b4dd3890cb44e1
Zvonimir <zvonimir.stracenski@fer.hr>
2025-11-13 00:16:59 +0100

    Promijenjena ruta indexa.

--- my-react-app/server.js
+++ my-react-app/server.js
@@ -409,10 +409,10 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'my-react-app/dist')));
+  app.use(express.static(path.join(__dirname, 'dist')));
 
   app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.join(__dirname, 'my-react-app/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 }
 
